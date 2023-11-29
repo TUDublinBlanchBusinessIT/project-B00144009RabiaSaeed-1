aeba1dddfd827b5e2ad0ee43c4249865ebdefd17
B00144009 Rabia Saeed <B00144009@mytudublin.ie>
2023-11-29 08:25:58 +0000

    okay here's the html

---
+++ CreateFlowerShop.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE flower_store;
+USE flower_store;
+
+CREATE TABLE flower (
+id INT AUTO_INCREMENT,
+first_Name VARCHAR(60),
+sur_Name VARCHAR(60),
+flower_Type VARCHAR(60),
+PRIMARY KEY(id)
+);
+
+INSERT INTO flower(first_Name, sur_Name, flower_Type) VALUES ('Rachel', 'Phillips', Roses);
