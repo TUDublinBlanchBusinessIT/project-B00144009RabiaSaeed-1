513add4dc899546b9c21acb998794c761ac43a1b
B00144009 Rabia Saeed <B00144009@mytudublin.ie>
2023-11-29 11:00:08 +0000

    sql file adds into database, baby step. Next is adding a fetch to get the rows from show file

--- ProcessFlowerShop.php
+++ ProcessFlowerShop.php
@@ -1,12 +1,10 @@
 <?php
 
-include ('dbcon.php');
-
 $firstName = $_POST['firstName'];
 $surName = $_POST['surName'];
 $flowerType = $_POST['flowerType'];
 
-echo "$firstName, $surName, $flowerType";
+include ('dbcon.php');
 
 $sql = "INSERT INTO orders (first_Name, sur_Name, flower_Type) VALUES ('$firstName', '$surName', '$flowerType')";
 
