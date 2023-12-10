a63a07b4cadaaba33ee52cf6a2f3e699c3e08ced
B00144009 Rabia Saeed <b00144009@mytudublin.ie>
2023-12-10 16:42:35 +0000

    added tables functional and session variables

--- ProcessFlowerShop.php
+++ ProcessFlowerShop.php
@@ -1,28 +1,54 @@
 <?php
-
-//getting the data fro post//
-$firstName = $_POST['firstName'];
-$surName = $_POST['surName'];
-$flowerType = $_POST['flowerType'];
-
-//$wrappingColor = $_POST['wrappingColor'];
-//$numOfFlowers = $_POST['numOfFlowers'];
-//$colorOfFlowers = $_POST['colorOfFlowers'];
-
 //connection to the database//
 include ('dbcon.php');
 
-//sql query//
-$sql = "insert into customer(first_Name, sur_Name, flower_Type) values ('$firstName', '$surName', '$flowerType')";
+//it was giving out to me about the timezone so I just added utc one//
+date_default_timezone_set('UTC');
 
-//$sql = "insert into bouquet(color_of_wrapping, number_of_flowers, color_of_flowers) values ('$wrappingColor', '$numOfFlowers', '$colorOfFlowers')";//
+//This starts the session//
+session_start();
 
-//executing sql query//
-mysqli_query($conn, $sql);
-//closing connection to db//
+//basically getting from the form itself//
+//post same as form, eg $firstName = $_POST['firstName']; firstName is from form//
+//getting the data from post//
+$firstName = $_POST['firstName'];
+$surName = $_POST['surName'];
+$dob = $_POST['dob'];
+
+//this looks at flowertype and assigns the first element to the dollar sign varaible. If it doesn't get assigned then its gonna be empty//
+if (isset($_POST['flowertype'][0])) {
+    $typeOfFlower = $_POST['flowertype'][0];
+} else {
+    $typeOfFlower = '';
+}
+//data from form again//
+$colorofwrapping = $_POST['wrappingColor'];
+$numberOfflowers = $_POST['numOfFlowers'];
+$colorOfFlowers = $_POST['colorOfFlowers'];
+//depending on whether it's 1 or 0. it's basically checking if specialdelivery in the post data//
+$SpecialDelivery = isset($_POST['SpecialDelivery']) ? 1 : 0;
+#$SpecialDelivery = isset($_POST['SpecialDelivery']) && $_POST['SpecialDelivery'] == '1';
+
+//The data that I'm getting from the form is stored in these session varaibles//
+$_SESSION['customerFirstName'] = $firstName;
+$_SESSION['customerSurName'] = $surName;
+$_SESSION['customerdob'] = $dob;
+$_SESSION['customerflowertype'] = $typeOfFlower;
+$_SESSION['customercolorofwrapping'] = $colorofwrapping;
+$_SESSION['customernumberOfflowers'] = $numberOfflowers;
+$_SESSION['customercolorOfFlowers'] = $colorOfFlowers;
+
+//this closes the connection to the database//
 mysqli_close($conn);
 
+//if the value is 1 then it's gonna go to that page basically//
+if ($SpecialDelivery === 1) {
+    header("Location: SpecialDelivery.html");
+}
 //echoing to screen that it got inserted into the database//
-echo "Information has been added"
+//echo "Information has been added";//
 
+//this shows a link to the file//
+$InventoryList = "inventoryList.php";
+echo "<a href='$InventoryList'>Open file for Tables</a>";
 ?>
--- sql/createDBV3.sql
+++ sql/createDBV3.sql
@@ -1,32 +1,37 @@
 #//database version 3//
 
-#CREATE DATABASE flower_store;
-#USE flower_store;
+drop database if exists flower_store;
+CREATE DATABASE flower_store;
+USE flower_store;
 
-#CREATE DATABASE flower_store;
-#USE flower_store;
+CREATE TABLE customer (
+    id INT AUTO_INCREMENT,
+    first_Name VARCHAR(60),
+    sur_Name VARCHAR(60),
+    d_o_b VARCHAR(60),
+    flower_Type VARCHAR(60),
+    PRIMARY KEY(id)
 
-#CREATE TABLE  (
-#id INT AUTO_INCREMENT,
-# VARCHAR(60),
-# VARCHAR(60),
-# VARCHAR(60),
-#PRIMARY KEY(id)
-#);
+);
 
-#INSERT INTO customer(, , ) VALUES ('', '', '');
+insert into customer(first_Name, sur_Name, d_o_b, flower_Type) values ('Rachel', 'Phillips', '1999-09-09', 'Roses');
 
+insert into customer(first_Name, sur_Name, d_o_b, flower_Type) values ('Tom', 'Smith', '2002-09-07', 'Lilies');
 
-#CREATE DATABASE flower_store;
-#USE flower_store;
+insert into customer(first_Name, sur_Name, d_o_b, flower_Type) values ('Emily', 'Watters', '2003-09-09', 'Peonies');
 
-#CREATE TABLE  (
-#id INT AUTO_INCREMENT,
-# VARCHAR(60),
-# VARCHAR(60),
-# VARCHAR(60),
-#PRIMARY KEY(id)
-#);
+CREATE TABLE bouquet(
+    id INT AUTO_INCREMENT,
+    color_of_wrapping VARCHAR(60),
+    number_of_flowers INT,
+    color_of_flowers VARCHAR(60),
+    customer_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(customer_id) REFERENCES customer(id)
+);
 
-#INSERT INTO customer(, , ) VALUES ('', '', '');
+insert into bouquet(color_of_wrapping, number_of_flowers, color_of_flowers, customer_id) values ( 'red', '4', 'yellow', '1');
 
+insert into bouquet(color_of_wrapping, number_of_flowers, color_of_flowers, customer_id) values ( 'orange', '8', 'blue', '2');
+
+insert into bouquet(color_of_wrapping, number_of_flowers, color_of_flowers, customer_id) values ( 'green', '9', 'red', '3');
